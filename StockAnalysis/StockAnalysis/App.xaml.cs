48b38123b7a56412afd34187d4343da8ffd650ab
António Melo <antonio.melo@xpand-it.com>
2018-12-12 17:45:26 +0000

    Starting charts with MicroCharts

--- StockAnalysis/StockAnalysis/App.xaml.cs
+++ StockAnalysis/StockAnalysis/App.xaml.cs
@@ -12,7 +12,7 @@ namespace StockAnalysis
         {
             InitializeComponent();
      
-            MainPage = new NavigationPage(new CompaniesStocksView());
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- StockAnalysis/StockAnalysis/Views/CompanyStockView.xaml.cs
+++ StockAnalysis/StockAnalysis/Views/CompanyStockView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Entry = Microcharts.Entry;
 
 namespace StockAnalysis.Views
 {
@@ -16,7 +19,35 @@ namespace StockAnalysis.Views
 		public CompanyStockView (String companySymbol)
 		{
             this.companySymbol = companySymbol;
-			InitializeComponent ();
-		}
+            List<Entry> entries = new List<Entry>
+            {
+                new Entry(200)
+                {
+                    Color = SKColor.Parse("#FF1493"),
+                    Label = "TEST1",
+                    ValueLabel = "200"
+
+                },
+                new Entry(50)
+                {
+                    Color = SKColor.Parse("#FF1493"),
+                    Label = "TEST2",
+                    ValueLabel = "50"
+
+                },
+                new Entry(100)
+                {
+                    Color = SKColor.Parse("#FF1493"),
+                    Label = "TEST3",
+                    ValueLabel = "100"
+
+                }
+            };
+            BindingContext = new { Symbol = companySymbol };
+
+            InitializeComponent ();
+
+            Chart1.Chart = new LineChart { Entries = entries };
+        }
 	}
 }
