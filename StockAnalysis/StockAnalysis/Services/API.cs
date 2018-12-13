cb79c9d17b36010ad26d4fb62b88cf4e383e6a1b
Nuno Ramos <nunor07.unfc@gmail.com>
2018-12-13 00:41:52 +0000

    Using new api to retrieve daily-quote of top10 companies

--- StockAnalysis/StockAnalysis/Services/API.cs
+++ StockAnalysis/StockAnalysis/Services/API.cs
@@ -11,51 +11,20 @@ namespace StockAnalysis.Services
 {
     static class API
     {
-        const string API_KEY = "0V27RLXB3DH4EYVO";
-        const string BASE_URL = "https://www.alphavantage.co/query?";
+        const string BASE_URL = "http://192.168.1.3:3000";
 
         public static async Task<List<CompanyStocks>> GetCompaniesStocks(String[] companies)
         {
+
             List<CompanyStocks> companyStocks = new List<CompanyStocks>();
-            List<Task> taskList = new List<Task>();
-            foreach (String company in companies)
+
+            using (var httpClient = new HttpClient())
             {
-                var task = new Task(async () => {
-                    using (var httpClient = new HttpClient())
-                    {
-                        Debug.WriteLine("começando");
-                        var jsonString = await httpClient.GetStringAsync(BASE_URL + "function=GLOBAL_QUOTE&symbol=" + company + "&apikey=" + API_KEY);
-                        var globalQuote = JsonConvert.DeserializeObject<GlobalQuote>(jsonString);
-                        companyStocks.Add(globalQuote.CompanyStocks);
-                        Debug.WriteLine("boas");
-                    }
-                });
-                taskList.Add(task);
-                task.Start();
+                var jsonString = await httpClient.GetStringAsync(BASE_URL + "/companies/daily-quote/" + "[\"" + string.Join("\", \"", companies) + "\"]");
+                companyStocks = JsonConvert.DeserializeObject<List<CompanyStocks>>(jsonString);
             }
-
-            Debug.WriteLine("vou esperar");
-
-            await Task.WhenAll(taskList.ToArray());
-
-            Debug.WriteLine("adeus");
 
             return companyStocks;
         }
-
-        public static async Task<CompanyStocks> GetCompanyStocks(String company)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                Debug.WriteLine("começando");
-                var jsonString = await httpClient.GetStringAsync(BASE_URL + "function=GLOBAL_QUOTE&symbol=" + company + "&apikey=" + API_KEY);
-                var globalQuote = JsonConvert.DeserializeObject<GlobalQuote>(jsonString);
-                //companyStocks.Add(globalQuote.CompanyStocks);
-                Debug.WriteLine("boas");
-
-                return globalQuote.CompanyStocks;
-            }
-        }
-
     }
 }
--- StockAnalysis/StockAnalysis/ViewsModels/CompanyStocksViewModel.cs
+++ StockAnalysis/StockAnalysis/ViewsModels/CompanyStocksViewModel.cs
@@ -12,47 +12,21 @@ namespace StockAnalysis.ViewsModels
     class CompanyStocksViewModel
     {
         public ObservableCollection<CompanyStocks> CompanyStocks { get; set; }
-        public String[] NASDAQCompanies = new string[] { "GOOG" };
-        //public String[] NASDAQCompanies = new string[] { "MSFT" };
+        public String[] NASDAQCompanies = new string[] { "MSFT", "AAPL", "AMZN", "GOOGL", "GOOG" };
 
         public CompanyStocksViewModel()
         {
             this.CompanyStocks = new ObservableCollection<CompanyStocks>();
-
-            /*this.CompanyStocks.Add(new CompanyStocks
-            {
-                Symbol = "GOOG",
-                AtualPrice = 10
-            });
-
-            this.CompanyStocks.Add(new CompanyStocks
-            {
-                Symbol = "MSF",
-                AtualPrice = 15
-            });*/
         }
 
         public async Task UpdateCompanyStocksAsync()
         {
-            /*var companyStocks = await API.GetCompanyStocks(NASDAQCompanies);
+            var companyStocks = await API.GetCompaniesStocks(NASDAQCompanies);
             this.CompanyStocks.Clear();
             companyStocks.ForEach((companyStock) =>
             {
                 this.CompanyStocks.Add(companyStock);
-            });*/
-            this.CompanyStocks.Clear();
-            List<Task> taskList = new List<Task>();
-            foreach (String company in NASDAQCompanies)
-            {
-                var task = new Task(async () => {
-                    CompanyStocks companyStocks = await API.GetCompanyStocks(company);
-                    this.CompanyStocks.Add(companyStocks);
-                });
-                taskList.Add(task);
-                task.Start();
-            }
-
-            await Task.WhenAll(taskList.ToArray());
+            });
         }
     }
 }
